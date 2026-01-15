c18a5155428ce5d25d3cbfe858014173269a6031
David M <david.maslov@student.adelaide.edu.au>
2026-01-16 00:11:52 +1030

    button anims

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,11 @@
 "use client"
 import { TrackballControls, useGLTF } from "@react-three/drei";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import { useRouter } from "next/navigation";
-import { useRef } from "react";
 import ModelShowcase from "./model-showcase";
 import styles from "./styles/homepage.module.scss";
 
 useGLTF.preload('/Duck.glb')
-
-function CameraLogger() {
-  const { camera } = useThree();
-  const lastPosition = useRef({ x: 0, y: 0, z: 0 });
-
-  useFrame(() => {
-    const pos = camera.position;
-    // Only log if position has changed
-    if (
-      Math.abs(pos.x - lastPosition.current.x) > 0.01 ||
-      Math.abs(pos.y - lastPosition.current.y) > 0.01 ||
-      Math.abs(pos.z - lastPosition.current.z) > 0.01
-    ) {
-      console.log(`Camera position: [${pos.x.toFixed(2)}, ${pos.y.toFixed(2)}, ${pos.z.toFixed(2)}]`);
-      lastPosition.current = { x: pos.x, y: pos.y, z: pos.z };
-    }
-  });
-
-  return null;
-}
 
 export default function Page() {
   const router = useRouter();
@@ -35,7 +14,6 @@ return  <>
             camera={{position:[6.65, 2.19, 4.15], fov: 50, near: 0.1, far: 100, zoom: 1.4}}
             >
             <ModelShowcase modelPath='/Duck.glb' />
-            <CameraLogger />
             <TrackballControls
             target={[0, 1, 0]}
             staticMoving={false}
@@ -45,7 +23,7 @@ return  <>
             />
         </Canvas>
     <div className={styles.homeWrapper}>
-      <p>Hi, I&apos;m David!</p>
+      <p>Hi, I&apos;m <strong>David</strong>!</p>
         <div className={styles.buttonRow}>
           <div className={styles.homeButton} onClick={() => router.push('/about')}><p>about_me</p></div>
           <div className={styles.homeButton} onClick={() => router.push('/projects')}><p>projects</p></div>
